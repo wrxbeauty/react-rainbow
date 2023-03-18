77c7aecf57563d2cbf328d949f9422381f521d31
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-18 13:53:29 -0400

    Added Bonus

--- src/components/ColorForm.js
+++ src/components/ColorForm.js
@@ -3,11 +3,19 @@ import React, { useState } from 'react'
 function ColorForm(props){
     let [input, setInput] = useState('')
 
+    const isColor = (strColor) => {
+        const s = new Option().style;
+        s.color = strColor;
+        return s.color !== '';
+    }
+
     const handleSubmit = (e) => {
         // We still want to eliminate the default behavior of our form submittal
         e.preventDefault()
         // addColor, the function we just built, should be available within props.
-        props.addColor(input)
+        if (isColor(input)) {
+            props.addColor(input)
+        }
         setInput('')
     }
 
